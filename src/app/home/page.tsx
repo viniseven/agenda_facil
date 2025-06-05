581dc8efa535c337e5f110040411b8d2b7422ad6
Marcus Vinicius <marcusv892@gmail.com>
2025-06-05 17:38:20 -0300

    feat: added session auth getsession

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,15 @@
-const HomePage = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+const HomePage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div>
       <h1>Olá Mundo</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.name}</p>
     </div>
   );
 };
