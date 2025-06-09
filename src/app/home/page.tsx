d4b64951d5159d338f337bcfd12b21ce97674502
Marcus Vinicius <marcusv892@gmail.com>
2025-06-09 19:44:40 -0300

    feat: added find data clinics

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,33 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
+import { PrismaClient } from "@/../../generated/prisma";
+
+const prisma = new PrismaClient();
+
 const HomePage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+  const clinics = await prisma.usersInClinics.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+    include: {
+      clinic: true,
+    },
+  });
+
+  console.log(clinics);
+
   return (
     <div>
       <h1>Olá Mundo</h1>
-      <p>{session?.user?.email}</p>
+      <p>
+        {clinics.map((clinic) => (
+          <li>{clinic.clinic.name}</li>
+        ))}
+      </p>
       <p>{session?.user?.name}</p>
     </div>
   );
