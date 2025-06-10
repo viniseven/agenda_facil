c770432deeabddffc6d7fe691005964bccf85e55
Marcus Vinicius <marcusv892@gmail.com>
2025-06-09 22:07:10 -0300

    feat: create homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,41 @@
-const HomePage = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { PrismaClient } from "../../../generated/prisma";
+import { redirect } from "next/navigation";
+
+import SignOutButton from "./components/button-signout";
+
+const prisma = new PrismaClient();
+
+const HomePage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
+  const clinics = await prisma.usersInClinics.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      clinic: true,
+    },
+  });
+
   return (
     <div>
       <h1>Olá Mundo</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.name}</p>
+      <p>
+        {clinics.map((clinic) => (
+          <p>{clinic.clinic.name}</p>
+        ))}
+      </p>
+      <SignOutButton />
     </div>
   );
 };
