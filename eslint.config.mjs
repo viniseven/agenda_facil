904508417b861cdcd77f7923e6b2d4c6d18049d2
Marcus Vinicius <marcusv892@gmail.com>
2025-06-12 15:38:54 -0300

    chore: added rules for eslint plugin simple import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,8 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
 import { dirname } from "path";
 
 const __dirname = dirname(__filename);
@@ -10,13 +13,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx", "**/*.mjs"],
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+    },
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
     },
   },
 ];
