439eb79182118ac13134d2ef5ecf3303e32d581a
Marcus Vinicius <marcusv892@gmail.com>
2025-06-14 22:05:50 -0300

    fix: auth better auth

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,5 @@
 import { toNextJsHandler } from "better-auth/next-js";
 
-import { auth } from "@/lib/auth"; // path to your auth file
+import { auth } from "@/lib/auth";
 
 export const { POST, GET } = toNextJsHandler(auth);
