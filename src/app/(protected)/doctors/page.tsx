531da97d1b91148212c6c5c253c0c8fdbd22433a
Marcus Vinicius <marcusv892@gmail.com>
2025-06-21 09:10:41 -0300

    feat: create doctors page component

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,5 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  PageContainer,
+  PageContent,
+  PageHeaderButtonActions,
+  PageHeaderContainer,
+  PageHeaderContent,
+  PageHeaderDescription,
+  PageHeaderTitle,
+} from "@/components/ui/page-container";
+import { Plus } from "lucide-react";
+
 const DoctorsPage = () => {
-  return <div>Olá</div>;
+  return (
+    <PageContainer>
+      <PageHeaderContainer>
+        <PageHeaderContent>
+          <PageHeaderTitle>Médicos</PageHeaderTitle>
+          <PageHeaderDescription>
+            Gerencie os médicos de sua clínica
+          </PageHeaderDescription>
+        </PageHeaderContent>
+
+        <PageHeaderButtonActions>
+          <Button>
+            <Plus />
+            Adicionar médico
+          </Button>
+        </PageHeaderButtonActions>
+      </PageHeaderContainer>
+      <PageContent>Olá</PageContent>
+    </PageContainer>
+  );
 };
 
 export default DoctorsPage;
