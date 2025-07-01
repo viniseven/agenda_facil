89702487dc16eb9e174b2af5759de8ada1ce4ca8
Marcus Vinicius <marcusv892@gmail.com>
2025-06-30 22:30:00 -0300

    feat: added avatar component

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
 import * as React from "react"
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
@@ -50,4 +50,4 @@ function AvatarFallback({
   )
 }
 
-export { Avatar, AvatarFallback,AvatarImage }
+export { Avatar, AvatarImage, AvatarFallback }
