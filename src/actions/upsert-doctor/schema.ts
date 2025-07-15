ee0db2da3a33343a3597a0a3a545f036c0056a01
Marcus Vinicius <marcusv892@gmail.com>
2025-07-14 21:29:47 -0300

    feat: create schema safe action

---
+++ src/actions/upsert-doctor/schema.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+
+export const upsertDoctorSchema = z
+  .object({
+    id: z.string().uuid().optional(),
+    name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+    speciality: z.string().trim(),
+    appointmentPriceInCents: z
+      .number()
+      .min(1, { message: "Preço da consulta é obrigatório" }),
+    availableFromWeekDay: z.number(),
+    availableToWeekDay: z.number(),
+    availableFromTime: z.string(),
+    availableToTime: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.availableToTime > data.availableFromTime;
+    },
+    {
+      message: "Horário de término não pode ser anterior ao horário de início",
+      path: ["availableToTime"],
+    },
+  );
+
+export type UpsertDoctorSchema = z.infer<typeof upsertDoctorSchema>;
